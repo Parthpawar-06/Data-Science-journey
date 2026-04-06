18c4cd3d2a1191ba222fc06806470f6c983ba2b7
Parthpawar-06 <96parthpawar@gmail.com>
2026-04-06 17:01:10 +0530

    Add 03_select_all.sql

---
+++ sql/hackerrank/basic-select/03_select_all.sql
@@ -0,0 +1,9 @@
+-- Problem: Select All
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Link: https://www.hackerrank.com/challenges/select-all-sql/problem
+--
+-- Task: Query all columns for every row in the CITY table.
+
+SELECT *
+FROM CITY;
