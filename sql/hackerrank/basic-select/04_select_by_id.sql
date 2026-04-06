35f54b40d7e53a987a26e833ace1ac0a3e366c88
Parthpawar-06 <96parthpawar@gmail.com>
2026-04-06 17:03:27 +0530

    Add HackerRank basic-select solution 04

---
+++ sql/hackerrank/basic-select/04_select_by_id.sql
@@ -0,0 +1,10 @@
+-- Problem: Select By ID
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Link: https://www.hackerrank.com/challenges/select-by-id/problem
+--
+-- Task: Query all columns for a city with ID = 1661.
+
+SELECT *
+FROM CITY
+WHERE ID = 1661;
