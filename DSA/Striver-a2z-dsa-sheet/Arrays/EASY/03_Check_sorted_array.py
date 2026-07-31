a5063cba186f648d511991cf20b8c9b12d436d01
Parthpawar-06 <96parthpawar@gmail.com>
2026-07-31 09:45:41 +0530

    Create 03_Check_sorted_array.py

---
+++ DSA/Striver-a2z-dsa-sheet/Arrays/EASY/03_Check_sorted_array.py
@@ -0,0 +1,48 @@
+#Question link : https://www.naukri.com/code360/problems/ninja-and-the-sorted-check_6581957
+# Problem statement
+# You have been given an array ‘a’ of ‘n’ non-negative integers.You have to check whether the given array is sorted in the non-decreasing order or not.
+# Your task is to return 1 if the given array is sorted. Else, return 0.
+# Example :
+# Input: ‘n’ = 5, ‘a’ = [1, 2, 3, 4, 5]
+# Output: 1
+# The given array is sorted in non-decreasing order; hence the answer will be 1.
+# Detailed explanation ( Input/output format, Notes, Images )
+# Sample Input 1 :
+# 4
+# 0 0 0 1
+# Sample Output 1 :
+# 1
+# Explanation For Sample Input 1 :
+# The given array is sorted in non-decreasing order; hence the answer will be 1. 
+# Sample Input 2 :
+# 5
+# 4 5 4 4 4
+# Sample Output 2 :
+# 0
+# Expected Time Complexity:
+# O(n), Where ‘n’ is the size of an input array ‘a’.
+# Constraints:
+# 1 ≤ 'n' ≤ 5*10^6
+# 0 ≤ 'a'[i] ≤ 10^9
+
+# Time limit: 1 sec
+
+# SOLUTION : BRUTE-FORCE : Time complexity O(n^2)
+def isSorted(n: int,  a: [int]) -> int:
+    # Write your code here.
+    n = len(a)
+    for i in range(n-1):
+        for j in range(i+1,n):
+            if a[i]>a[j]:
+                return 0
+    return 1
+# OPTIMISED SOLUTION : TIME COMPLEXITY O(n)
+
+def isSorted(n: int,  a: [int]) -> int:
+    # Write your code here.
+    n = len(a)
+    for i in range(0,n-1):
+        if (a[i]>a[i+1]):
+            return 0
+        
+    return 1
