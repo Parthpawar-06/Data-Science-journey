31d98e6c28eb704b8136e2ed39880b26378ce25d
Parthpawar-06 <96parthpawar@gmail.com>
2026-08-01 11:23:24 +0530

    Create 07_Move_zeroes.py

---
+++ DSA/Striver-a2z-dsa-sheet/Arrays/EASY/07_Move_zeroes.py
@@ -0,0 +1,56 @@
+# Question Link: https://leetcode.com/problems/move-zeroes/
+# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+# Note that you must do this in-place without making a copy of the array.
+# Example 1:
+# Input: nums = [0,1,0,3,12]
+# Output: [1,3,12,0,0]
+
+# Example 2:
+# Input: nums = [0]
+# Output: [0]
+# Constraints:
+# 1 <= nums.length <= 104
+# -231 <= nums[i] <= 231 - 1
+# Follow up: Could you minimize the total number of operations done?
+
+#Solution 1 : 
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        non_zero = 0
+        for i in range(len(nums)):
+            if(nums[i]!=0):
+                nums[non_zero] = nums[i]
+                non_zero+=1
+        for i in range(non_zero,len(nums)):
+            nums[i] = 0
+
+        return nums
+
+#Solution 2:
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        i = 0
+        for j in range(n):
+            if(nums[j]!=0):
+                nums[i],nums[j]=nums[j],nums[i]
+                i+=1
+        return nums
+                
+
+            
+                    
+
+                
+                
+
+            
+                    
+
+                
