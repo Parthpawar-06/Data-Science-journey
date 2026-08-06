949605801fc8e63aa1738e6c8f0a3e2d1029f009
Parthpawar-06 <96parthpawar@gmail.com>
2026-08-06 17:39:43 +0530

    Create 04_Longest_Common_Prefix.py

---
+++ DSA/Striver-a2z-dsa-sheet/STRINGS/EASY/04_Longest_Common_Prefix.py
@@ -0,0 +1,34 @@
+#Question Link : https://leetcode.com/problems/longest-common-prefix/
+# Write a function to find the longest common prefix string amongst an array of strings.
+# If there is no common prefix, return an empty string "".
+# Example 1:
+# Input: strs = ["flower","flow","flight"]
+# Output: "fl"
+# Example 2:
+# Input: strs = ["dog","racecar","car"]
+# Output: ""
+# Explanation: There is no common prefix among the input strings.
+# Constraints:
+# 1 <= strs.length <= 200
+# 0 <= strs[i].length <= 200
+# strs[i] consists of only lowercase English letters if it is non-empty.
+
+#Solution :
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+
+        if not strs:
+            return ""
+
+        strs.sort()
+        first = strs[0]
+        last = strs[-1]
+
+        ans = []
+        for i in range(min(len(first),len(last))):
+            if first[i]!=last[i]:
+                return "".join(ans)
+            ans.append(first[i])
+        return "".join(ans)
+
+
